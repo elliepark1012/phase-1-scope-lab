58cdedde788815df7a7c1a5addc305ba0b30eaff
Ellie <elliepark1012@gmail.com>
2022-07-05 19:21:32 -0400

    commit tree 4a2239d293b8c1242ec8affb7d703e18f6ed6a34

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    return customerName = customerName.toUpperCase() 
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;  
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+const leastFavoriteCustomer = 'no one'
+
+function changeLeastFavoriteCustomer() {
+   leastFavoriteCustomer = 'I don\'t want to pick one'
+    return leastFavoriteCustomer
+}
